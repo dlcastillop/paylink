afd875bb0921fa1961758da24392f8a559643f5d
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2022-09-16 09:30:56 -0400

    changing project estructure to use themes

--- src/App.jsx
+++ src/App.jsx
@@ -15,68 +15,24 @@ import qvapay from "./images/crypto/qvapay.svg";
 function App() {
   return (
     <>
-      <Hero
-        src={imageHero}
-        alt="Photo of Daniel Castillo"
-        name="Daniel Castillo"
-        description="Frontend web developer"
-      />
+      <div class="minimalist-theme">
+        <Hero src={imageHero} alt="Photo of Daniel Castillo" name="Daniel Castillo" description="Frontend web developer" />
 
-      <section id="social-media" className="d-flex justify-content-center mb-5">
-        {/* Social media components are placed here */}
-        <section id="social-media" className="...">
-          <SocialMedia
-            img={link}
-            label="Link"
-            href="https://danielcastillop.netlify.app"
-          />
-          <SocialMedia
-            img={github}
-            label="GitHub"
-            href="https://github.com/dlcastillop"
-          />
-          <SocialMedia
-            img={twitter}
-            label="Twitter"
-            href="https://twitter.com/dlcastillop"
-          />
-          <SocialMedia
-            img={linkedin}
-            label="LinkedIn"
-            href="https://linkedin.com/in/dlcastillop"
-          />
-          <SocialMedia
-            img={instagram}
-            label="Instagram"
-            href="https://instagram.com/dlcastillop"
-          />
+        <section id="social-media" className="d-flex justify-content-center mb-5">
+            <SocialMedia img={link} label="Link" href="https://danielcastillop.netlify.app" />
+            <SocialMedia img={github} label="GitHub" href="https://github.com/dlcastillop" />
+            <SocialMedia img={twitter} label="Twitter" href="https://twitter.com/dlcastillop" />
+            <SocialMedia img={linkedin} label="LinkedIn" href="https://linkedin.com/in/dlcastillop" />
+            <SocialMedia img={instagram} label="Instagram" href="https://instagram.com/dlcastillop" />
         </section>
-      </section>
 
-      <section
-        id="payment-options"
-        className="d-flex flex-column align-items-center pb-5"
-      >
-        {/* Payment components are placed here */}
-        <Payment
-          img={btc}
-          label="Bitcoin"
-          value="bc1qejahgjmqnmyrw7jvn0d7evfdq6ssjl3wq7hfle"
-          id="btc"
-        />
-        <Payment
-          img={eth}
-          label="Ethereum"
-          value="0x59C864f658caD68A19a97499755080c056079988"
-          id="eth"
-        />
-        <Payment
-          img={qvapay}
-          label="QvaPay"
-          value="https://qvapay.com/payme/dlcastillop"
-          id="qvapay"
-        />
-      </section>
+        <section id="payment-options" className="d-flex flex-column align-items-center pb-5">
+          {/* Payment components are placed here */}
+          <Payment img={btc} label="Bitcoin" value="bc1qejahgjmqnmyrw7jvn0d7evfdq6ssjl3wq7hfle" id="btc" />
+          <Payment img={eth} label="Ethereum" value="0x59C864f658caD68A19a97499755080c056079988" id="eth" />
+          <Payment img={qvapay} label="QvaPay" value="https://qvapay.com/payme/dlcastillop" id="qvapay" />
+        </section>
+      </div>
     </>
   );
 }
