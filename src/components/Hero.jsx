84237be226d07b0dea67c95c92e8f239d899afc8
Daniel Castillo <dlcastillo3015@gmail.com>
2023-02-19 11:28:53 -0500

    fix: replace boostrap classes with sass

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,10 @@
 const Hero = (props) => {
   return (
-    <section className="d-flex flex-column align-items-center pt-2 mb-1">
-      <img src={props.src} alt={props.alt} className="mb-1 hero-image my-4" />
-      <div className="pt-3">
-        <h3 className="mb-1 text-center">{props.name}</h3>
-        <p className="text-center text-center">{props.description}</p>
+    <section className="flex-column-center pt--2 mb--1">
+      <img src={props.src} alt={props.alt} className="mb--1 hero-image my--4" />
+      <div className="pt--3">
+        <h3 className="mb--1 text--center">{props.name}</h3>
+        <p className="text--center">{props.description}</p>
       </div>
     </section>
   );
