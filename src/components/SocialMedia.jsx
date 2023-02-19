b0d9a0d3e6c0f314683dc89241b8bb35dc233631
Daniel Castillo <dlcastillo3015@gmail.com>
2023-02-19 13:39:53 -0500

    fix: replace boostrap classes with sass in social media component

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -2,7 +2,7 @@ const SocialMedia = ({ value, href, label }) => {
   let icon = "social-icon bx " + value;
 
   return (
-    <a href={href} target="_blank" className="ms-2 me-2" title={label}>
+    <a href={href} target="_blank" className="ms--2 me--2" title={label}>
       <i className={icon}></i>
     </a>
   );
