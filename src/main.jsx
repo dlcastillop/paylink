011dd0fad1b85ba739f52e4bbb61a2356b26fdd6
Daniel Castillo <dlcastillo3015@gmail.com>
2023-03-16 17:13:36 -0400

    initial setup of react, tailwind and daisy-ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import Config from "./paylink.config.json";
-import "./assets/main.scss";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-let title = "PayLink | Pay or donate to " + Config.properties.name;
-
-// Set title tag
-document.title = title;
-
-// Set meta tag (title)
-let meta = document.querySelectorAll("meta")[2];
-meta.setAttribute("content", title);
-
-// Set meta tag (og:url and title)
-meta = document.querySelectorAll("meta")[5];
-meta.setAttribute("og:url", window.location);
-
-meta = document.querySelectorAll("meta")[6];
-meta.setAttribute("og:title", title);
-
-// Set meta tag (twitter:url and twitter:title)
-meta = document.querySelectorAll("meta")[10];
-meta.setAttribute("twitter:url", window.location);
-
-meta = document.querySelectorAll("meta")[11];
-meta.setAttribute("twitter:title", title);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // If you are deploying on GitHub Pages, you need to set the
-  // base to the name of your repository except if the repository
-  // is named <username>.github.io, in that case, you can leave
-  // the line commented or set the base to '/'
-  // base: '/paylink/',
-  plugins: [react()]
+  plugins: [react()],
 })
