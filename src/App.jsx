2ba1c80db27f0266aa7767fac5f2d763dc617da7
Daniel Castillo <dlcastillo3015@gmail.com>
2023-03-18 17:02:45 -0400

    fix: add meta tags

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,28 @@ import Config from "./paylink.config.json";
 
 const App = () => {
   const [qrData, setQrData] = useState({ img: "", label: "", value: "" });
+  const title = "PayLink | Pay or donate to " + Config.properties.name;
+
+  // Set title tag
+  document.title = title;
+
+  // Set meta tag (title)
+  let meta = document.querySelectorAll("meta")[2];
+  meta.setAttribute("content", title);
+
+  // Set meta tag (og:url and title)
+  meta = document.querySelectorAll("meta")[5];
+  meta.setAttribute("og:url", window.location);
+
+  meta = document.querySelectorAll("meta")[6];
+  meta.setAttribute("og:title", title);
+
+  // Set meta tag (twitter:url and twitter:title)
+  meta = document.querySelectorAll("meta")[10];
+  meta.setAttribute("twitter:url", window.location);
+
+  meta = document.querySelectorAll("meta")[11];
+  meta.setAttribute("twitter:title", title);
 
   return (
     <div className="max-w-xl mx-auto items-center justify-center">
